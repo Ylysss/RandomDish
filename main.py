2cac9f871e355ab0b502e96f1a03761be69e3fc7
Ylisss <lusikS@gmx.de>
2022-08-31 21:54:05 +0200

    Variable "Erdbeeren" definiiert Funktion "Zeige Hintergrund" angelegt

--- main.py
+++ main.py
@@ -2,9 +2,16 @@
 from PIL import Image
 
 # Hintergrundbild definieren (Variable "Hintergrund" definieren = das konkrete Bild hinterlegt)
-hintergrund = Image.open("Bilder/Plate.png")
+hintergrund = Image.open("Bilder/teller.png")
 
 # Hintergrund rendern (unser Image-objekt anzeigen): Methode "show" durch Schreiben des Punktes aufrufen, das angezeigte Fenster benennen "Mein Gerincht"
+#hintergrund.show("Mein Gericht")
+
+Erdbeeren = Image.open("Bilder/Erdbeeren.png")
+
+hintergrund.paste(Erdbeeren, (200,200))
+
 hintergrund.show("Mein Gericht")
+
 
 
