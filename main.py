a0ca993c76598d23fd535e094c441f1b4ce5faa1
Elias Tatros <elias.tatros@capgemini.com>
2022-08-31 22:13:10 +0200

    Plan

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 # Klasse Image aus der Bibliothek Pillow(PIL) importiert (auf Python Packages gehen, im Python Package Index nach der Bibliothek image suchen, darin Klasse Image)
 from PIL import Image
 
+# Hauptgericht.txt öffnen
+#   - Neues Dictionary anlegen
+#   - Zeile für Zeile einlesen
+#   - Funktion split nutzen, um Zeile bei "," zu trennen
+#   - Neuen Dictionary Eintrag mit (key=name,value=pfad) hinzufügen
+# Dann das gleiche für BeilageA.txt, BeilageB.txt, BeilageC.txt
+
 # Hintergrundbild definieren (Variable "Hintergrund" definieren = das konkrete Bild hinterlegt)
 hintergrund = Image.open("Bilder/teller.png")
 
@@ -9,7 +16,7 @@ hintergrund = Image.open("Bilder/teller.png")
 
 Erdbeeren = Image.open("Bilder/Erdbeeren.png")
 
-hintergrund.paste(Erdbeeren, (200,200))
+hintergrund.paste(Erdbeeren, (200,200), Erdbeeren)
 
 hintergrund.show("Mein Gericht")
 
